5010cb39ffeea835029b90147c237511a5e5a1a2
Manezki <manezki@gmail.com>
2017-12-22 17:32:53 +0200

    Tweet Preprocessing. Requires writing

--- src/tweet_preprocess.py
+++ src/tweet_preprocess.py
@@ -1,5 +1,5 @@
-
-
+from os import path as op
+import re
 
 
 # Notes from Mathieu Cliche https://arxiv.org/pdf/1704.06125.pdf,
@@ -10,3 +10,23 @@
     - Any letter repeated more than 2 times in a row, replaced by 2 repetitions. E.g "cooool" -> "cool"
     - All tweets lowercased
 """
+
+DATADIR = op.join(op.dirname(__file__), "..", "data", "4a-english", "4A-English")
+DATAFILE = op.join(DATADIR, "SemEval2017-task4-dev.subtask-A.english.INPUT.txt")
+OUTFILE = op.join(DATADIR, "SemEval2017-task4-dev.subtask-A.english.INPUT_PREPROS.txt")
+
+C = 0
+
+def rept(matchobj):
+    print("MATHCOBJ: " + matchobj.groups(0)[0])
+    return str(matchobj.groups(0)[0] + matchobj.groups(0)[0])
+
+with open(DATAFILE, "r") as reader:
+    line = reader.readline()
+    while line != None and C <= 100:
+        line = re.sub(r'http:\/\/t\.co\/([A-Z0-9+&@#\/%=~_|$]*)',
+                  "<URL>", line, flags=re.IGNORECASE)
+        line = re.sub(r'([a-zA-Z])\1{2,}', rept, line)
+        print(line)
+        line = reader.readline()
+        C += 1
