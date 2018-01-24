1c56df3621f54cf5b9ef7bb332ffed15f23cfc0a
Manezki <manezki@gmail.com>
2018-01-24 22:40:21 +0200

    Setup.py base

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='TwitMine',
+    version='0.2dev',
+    packages=['twitmine',],
+    license='Apache 2.0',
+    long_description=open('README.md').read(),
+)
